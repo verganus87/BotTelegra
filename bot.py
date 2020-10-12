732ce385243995ebba06ed62c554c48bc4ba7a2e
Verganus <57346804+verganus87@users.noreply.github.com>
2020-10-12 18:05:33 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,22 @@
 import telebot
-import congig
+while True:
+    try:
+        bot = telebot.TeleBot('1259786699:AAH0F8jicygEYUAXabb0MeO3YY78vS9v9lc')
 
+        @bot.message_handler(commands=['start'])
+        def start_message(message):
+            bot.send_message(message.chat.id, 'Привет, посмотри на описание бота и напиши любое сообшение)))')
+
+        @bot.message_handler(commands=['starting'])
+        def starting_message(message):
+            bot.send_message(704752422, 'Нажми /start')
+
+        @bot.message_handler(content_types=['text'])
+        def send_message(message):
+            bot.send_message(message.chat.id, 'Ты королева вселенной 💕')
+            bot.send_message(704752422, message.text)
+
+        bot.polling()
+    except:
+        pass
+        
